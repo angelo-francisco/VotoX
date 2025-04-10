c3d79f2f722597883ec455c3458ab27b7db897bb
angelo-francisco <ics20080729@gmail.com>
2025-04-10 15:35:20 +0100

    more one

--- votes/urls.py
+++ votes/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import (
     home_page,
+    pollings
 )
 
 urlpatterns = [
     path('', home_page, name="home_page"),
+    path("pollings/", pollings, name="pollings")
 ]
--- votes/views.py
+++ votes/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home_page(request):
     return render(request, 'votes/home_page.html')
+
+
+def pollings(request):
+    return render(request, 'votes/pollings.html')
