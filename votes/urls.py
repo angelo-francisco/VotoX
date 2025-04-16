f433785c2132d9771aa2344b4711a10ef33e7062
angelo-francisco <ics20080729@gmail.com>
2025-04-16 16:27:33 +0100

    add new path to urlpatterns

--- votes/urls.py
+++ votes/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import home_page, manage_comments, new_poll, poll_detail, pollings
+from .views import go_poll, home_page, manage_comments, new_poll, poll_detail, pollings
 
 urlpatterns = [
     path("", home_page, name="home_page"),
     path("pollings/", pollings, name="pollings"),
+    path("new/", new_poll, name="new_poll"),
+    path("go/", go_poll, name="go_poll"),
     path("@<username>/<poll_slug>/", poll_detail, name="poll_detail"),
-    path("new-poll/", new_poll, name="new_poll"),
     path("manage-comments/<poll_slug>/", manage_comments, name="manage_comments"),
 ]
